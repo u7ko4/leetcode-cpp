0d8b1cfeabd113ace3519adf9cdb27c2db9e7756
keith <ke.struct@gmail.com>
2020-09-30 15:47:44 +0800

    21. Merge Two Sorted Lists

---
+++ 21. Merge Two Sorted Lists/solution.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <vector>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        ListNode *root = new ListNode();
+        ListNode *head = root;
+        while (l1 != nullptr || l2 != nullptr) {
+            if (l1 != nullptr && l2 != nullptr) {
+                if (l1->val <= l2->val) {
+                    head->next = l1;
+                    l1 = l1->next;
+                } else {
+                    head->next = l2;
+                    l2 = l2->next;
+                }
+            } else {
+                if (l1 != nullptr) {
+                    head->next = l1;
+                    l1 = l1->next;
+                } else {
+                    head->next = l2;
+                    l2 = l2->next;
+                }
+            }
+            head = head->next;
+        }
+        return root->next;
+    }
+};
