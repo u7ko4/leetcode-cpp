145c5eda3829cee26495ec0242b68f35c48fde90
keith <ke.struct@gmail.com>
2020-11-01 20:04:22 +0800

    35. Search Insert Position

---
+++ 35. Search Insert Position/solution.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <array>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int searchInsert(vector<int> &nums, int target) {
+        if (nums.back() < target) return nums.size();
+        int l = 0, r = nums.size();
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] < target) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+        return r;
+    }
+};
