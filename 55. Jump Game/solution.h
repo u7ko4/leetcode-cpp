e438c6b29ad2b7bbdc4a666724f352db9a23b8a1
keith <ke.struct@gmail.com>
2020-11-29 19:23:09 +0800

    55. Jump Game

---
+++ 55. Jump Game/solution.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <array>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+
+class Solution {
+public:
+    bool canJump(vector<int> &nums) {
+        vector<int> dp(nums.size(), 0);
+        for (int i = 1; i < nums.size(); ++i) {
+            dp[i] = max(dp[i - 1], nums[i - 1]) - 1;
+            if (dp[i] < 0) return false;
+        }
+        return true;
+    }
+};
