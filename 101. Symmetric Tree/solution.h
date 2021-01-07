1e280c6e01a23be44343ee6bd58ba8b1cc994215
keith <ke.struct@gmail.com>
2021-01-07 20:40:25 +0800

    101. Symmetric Tree

---
+++ 101. Symmetric Tree/solution.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <array>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode *left, TreeNode *right) {
+        if (!left && !right) return true;
+        if (left && !right) return false;
+        if (!left && right) return false;
+        if (left->val != right->val) return false;
+        return isSymmetric(left->left, right->right) && isSymmetric(left->right, right->left);
+    }
+
+    bool isSymmetric(TreeNode *root) {
+        if (!root) return true;
+        return isSymmetric(root->left, root->right);
+    }
+};
