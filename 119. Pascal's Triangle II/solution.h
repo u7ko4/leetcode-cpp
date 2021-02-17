ec5f4a9c9277d357cbdab743c6370b89915285ed
keith <ke.struct@gmail.com>
2021-02-17 11:28:37 +0800

    119. Pascal's Triangle II

---
+++ 119. Pascal's Triangle II/solution.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <array>
+#include <algorithm>
+#include <set>
+#include <queue>
+#include <cstdlib>
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> res(rowIndex + 1);
+        res[0] = 1;
+        for (int i = 1; i <= rowIndex; ++i) {
+            for (int j = i; j >= 1; --j) {
+                res[j] += res[j - 1];
+            }
+        }
+        return res;
+    }
+};
