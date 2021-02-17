f07159da41cebe683b9c67b3ed55a1efd4e4a157
keith <ke.struct@gmail.com>
2021-02-17 11:40:16 +0800

    122. Best Time to Buy and Sell Stock II

---
+++ 122. Best Time to Buy and Sell Stock II/solution.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <array>
+#include <algorithm>
+#include <set>
+#include <queue>
+#include <cstdlib>
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        int res = 0;
+        for (int i = 0; i < prices.size() - 1; ++i) {
+            if (prices[i] < prices[i + 1]) {
+                res += prices[i + 1] - prices[i];
+            }
+        }
+        return res;
+    }
+};
